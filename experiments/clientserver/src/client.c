f47099a846d82e9cdac95c2944281e1a72a59f74
Sihlfall <jaybee@sihlfall.ch>
2025-03-18 08:36:57 +0100

    Simple client/server, mainly created by ChatGPT

---
+++ experiments/clientserver/src/client.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char *message = "a";
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Send message to server
+    send(sock, message, strlen(message), 0);
+    printf("Message sent: %s\n", message);
+
+    // Read server response
+    int valread = read(sock, buffer, BUFFER_SIZE);
+    if (valread > 0) {
+        printf("Server response: %s\n", buffer);
+    }
+
+    // Close socket
+    close(sock);
+
+    return 0;
+}
